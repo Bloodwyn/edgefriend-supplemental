59413e1598ce50ee67fb1bd62414d56bbd3b8571
Bastian K <bastian.kuth@hs-coburg.de>
2023-07-03 10:53:07 +0200

    remove broken submodules

--- rapidobj/cmake/RapidObjConfig.cmake
+++
@@ -1,6 +0,0 @@
-if (CMAKE_SYSTEM_NAME MATCHES "Linux")
-    list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
-    find_package(Threads REQUIRED)
-endif()
-
-include(${CMAKE_CURRENT_LIST_DIR}/RapidObj.cmake)
